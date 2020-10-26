1554c621fee5c1a01e47216bcf1d5db26ddaf527
yu myeong cheol <mspringmspring@naver.com>
2020-10-26 15:46:36 +0900

    [ADD]

--- src/main/java/addad/api/domain/payload/request/SignIn.java
+++ src/main/java/addad/api/domain/payload/request/SignIn.java
@@ -4,10 +4,12 @@ package addad.api.domain.payload.request;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.validation.constraints.Email;
+
 @Getter
 @Setter
 public class SignIn {
-
+    @Email
     private String email;
 
     private String password;
