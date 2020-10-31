9f29f824804e1b27109f47426190a1506cd7d4e2
yu myeong cheol <mspringmspring@naver.com>
2020-11-01 00:32:17 +0900

    [CREATE] email, code request

---
+++ src/main/java/addad/api/domain/payload/request/VerifyCodeRequest.java
@@ -0,0 +1,18 @@
+package addad.api.domain.payload.request;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+
+@Getter
+@Setter
+public class VerifyCodeRequest {
+
+    @Email
+    private String email;
+
+    @NotBlank
+    private String code;
+}
