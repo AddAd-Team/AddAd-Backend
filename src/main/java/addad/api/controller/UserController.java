fa2f69e9aa13d696c5f7d4fa4edd09f3381344e1
yu myeong cheol <mspringmspring@naver.com>
2020-11-01 00:28:16 +0900

    [CREATE] user controller

--- src/main/java/addad/api/controller/UserController.java
+++ src/main/java/addad/api/controller/UserController.java
@@ -1,4 +1,22 @@
-package addad.api.controller;
-
-public class UserController {
-}
+//package addad.api.controller;
+//
+//import addad.api.service.user.UserService;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.web.bind.annotation.PostMapping;
+//import org.springframework.web.bind.annotation.RequestBody;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//import javax.validation.Valid;
+//
+//@RestController
+//@RequestMapping("/api/user")
+//@RequiredArgsConstructor
+//public class UserController {
+//    private final UserService userService;
+//
+//    @PostMapping(value = "/signup")
+//    public void signIn(@RequestBody @Valid SignUp signUp) {
+//        userService.signUp(signUp);
+//    }
+//}
