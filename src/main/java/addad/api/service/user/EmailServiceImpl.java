0e8b47b0e28a468f96e4ef01dc892ea631b52cfa
mspring03 <aespringaa@gmail.com>
2020-11-01 21:04:24 +0900

    [MODIFY]

--- src/main/java/addad/api/service/user/EmailServiceImpl.java
+++ src/main/java/addad/api/service/user/EmailServiceImpl.java
@@ -59,7 +59,7 @@ public class EmailServiceImpl implements EmailService {
         SimpleMailMessage mailMessage = new SimpleMailMessage();
         mailMessage.setFrom(adminEmail);
         mailMessage.setTo(email);
-        mailMessage.setSubject("혼치페이 이메일 인증입니다.");
+        mailMessage.setSubject("AddAd 이메일 인증입니다.");
         mailMessage.setText("계정 인증을 위한 코드는 " + code + "입니다.");
         javaMailSender.send(mailMessage);
     }
