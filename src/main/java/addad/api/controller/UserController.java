b7de114e8f0a8afc8cc64e48f11ed5e9e6a8709f
mspring03 <aespringaa@gmail.com>
2020-11-01 21:03:59 +0900

    [CREATE] user controller

--- src/main/java/addad/api/controller/UserController.java
+++ src/main/java/addad/api/controller/UserController.java
@@ -1,22 +1,29 @@
-//package addad.api.controller;
-//
-//import addad.api.service.user.UserService;
-//import lombok.RequiredArgsConstructor;
-//import org.springframework.web.bind.annotation.PostMapping;
-//import org.springframework.web.bind.annotation.RequestBody;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RestController;
-//
-//import javax.validation.Valid;
-//
-//@RestController
-//@RequestMapping("/api/user")
-//@RequiredArgsConstructor
-//public class UserController {
-//    private final UserService userService;
-//
-//    @PostMapping(value = "/signup")
-//    public void signIn(@RequestBody @Valid SignUp signUp) {
-//        userService.signUp(signUp);
-//    }
-//}
+package addad.api.controller;
+
+import addad.api.domain.payload.request.VerifyCodeRequest;
+import addad.api.domain.payload.request.userEmail;
+import addad.api.service.user.EmailService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
+
+@RestController
+@RequestMapping("/api/user")
+@RequiredArgsConstructor
+public class UserController {
+    //private final UserService userService;
+    private final EmailService emailService;
+
+    @PostMapping(value = "/emailSender")
+    public void EmailSender(@RequestBody() @NotNull @Email userEmail email) {
+        emailService.sendEmail(email.getEmail());
+    }
+
+    @PutMapping(value = "/emailAuth")
+    public void verifyEmail(@RequestBody @Valid VerifyCodeRequest verifyCodeRequest) {
+        emailService.verifyEmail(verifyCodeRequest);
+    }
+}
