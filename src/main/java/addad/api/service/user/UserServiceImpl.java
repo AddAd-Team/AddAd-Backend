a4cb5ed5f3486dd1edfd91cb24b88bdbfcc5a6a3
mspring03 <aespringaa@gmail.com>
2020-11-02 15:04:45 +0900

    [ADD] signup

--- src/main/java/addad/api/service/user/UserServiceImpl.java
+++ src/main/java/addad/api/service/user/UserServiceImpl.java
@@ -1,5 +1,27 @@
 package addad.api.service.user;
 
-public class UserServiceImpl {
+import addad.api.domain.entities.User;
+import addad.api.domain.payload.request.SignUp;
+import addad.api.domain.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import addad.api.utils.PasswordEncoder;
 
+
+@Service
+@RequiredArgsConstructor
+public class UserServiceImpl implements UserService {
+    private final UserRepository userRepository;
+
+    @Override
+    public void signUp(SignUp signUp) {
+        userRepository.save(
+                User.builder()
+                        .userEmail(signUp.getEmail())
+                        .userPw(PasswordEncoder.encode(signUp.getPassword()))
+                        .userName(signUp.getName())
+                        .hashtag(signUp.getTag())
+                        .build()
+        );
+    }
 }
