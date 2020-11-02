1fb3bdc02c6397a14abcab672948d645cd41ffd0
mspring03 <aespringaa@gmail.com>
2020-11-02 15:17:50 +0900

    [ADD] userinfo value

--- src/main/java/addad/api/service/user/UserServiceImpl.java
+++ src/main/java/addad/api/service/user/UserServiceImpl.java
@@ -1,6 +1,7 @@
 package addad.api.service.user;
 
 import addad.api.domain.entities.User;
+import addad.api.domain.entities.enums.Userinfo;
 import addad.api.domain.payload.request.SignUp;
 import addad.api.domain.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
@@ -21,6 +22,7 @@ public class UserServiceImpl implements UserService {
                         .userPw(PasswordEncoder.encode(signUp.getPassword()))
                         .userName(signUp.getName())
                         .hashtag(signUp.getTag())
+                        .userinfo(Userinfo.valueOf(signUp.getUserinfo()))
                         .build()
         );
     }
