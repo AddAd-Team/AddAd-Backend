7dd614e47b105fd3d56a8827c83cb2b73dd9715f
Kiwon <parkkiwon0424@gmail.com>
2020-11-03 16:08:07 +0900

    fix parameter

--- src/main/java/addad/api/service/post/PostService.java
+++ src/main/java/addad/api/service/post/PostService.java
@@ -3,5 +3,5 @@ package addad.api.service.post;
 import addad.api.domain.payload.request.PostRequest;
 
 public interface PostService {
-    void write(PostRequest postWriteRequest);
+    void write(PostRequest postRequest);
 }
