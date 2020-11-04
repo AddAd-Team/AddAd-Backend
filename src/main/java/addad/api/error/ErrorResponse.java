c17d49d9bd03cdb7d781c754e61c369add2afcdc
mspring03 <aespringaa@gmail.com>
2020-11-05 02:46:52 +0900

    [CREATE] error response

---
+++ src/main/java/addad/api/error/ErrorResponse.java
@@ -0,0 +1,14 @@
+package addad.api.error;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class ErrorResponse {
+
+    private int status;
+
+    private String message;
+
+}
