74d26195c05b4f6c2ebe6501af9a9d28000badaa
mspring03 <aespringaa@gmail.com>
2020-11-05 03:50:36 +0900

    [ADD] profile controller

--- src/main/java/addad/api/service/mypage/MypageService.java
+++ src/main/java/addad/api/service/mypage/MypageService.java
@@ -1,6 +1,9 @@
 package addad.api.service.mypage;
 
+import addad.api.domain.payload.response.ProfileResponse;
+
 public interface MypageService {
     void passwordAuth (String Password);
     void passwordChange (String Password);
+    ProfileResponse profile ();
 }
