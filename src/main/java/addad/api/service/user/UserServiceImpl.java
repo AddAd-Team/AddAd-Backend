6e10a879047433e39cfa8babc9ad8b0a711991ed
mspring03 <aespringaa@gmail.com>
2020-11-05 02:49:45 +0900

    [MODIFY]

--- src/main/java/addad/api/service/user/UserServiceImpl.java
+++ src/main/java/addad/api/service/user/UserServiceImpl.java
@@ -5,21 +5,22 @@ import addad.api.domain.entities.enums.Userinfo;
 import addad.api.domain.payload.request.SignUp;
 import addad.api.domain.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
-import addad.api.utils.PasswordEncoder;
 
 
 @Service
 @RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
     private final UserRepository userRepository;
+    private  final PasswordEncoder passwordEncoder;
 
     @Override
     public void signUp(SignUp signUp) {
         userRepository.save(
                 User.builder()
                         .userEmail(signUp.getEmail())
-                        .userPw(PasswordEncoder.encode(signUp.getPassword()))
+                        .userPw(passwordEncoder.encode(signUp.getPassword()))
                         .userName(signUp.getName())
                         .hashtag(signUp.getTag())
                         .userinfo(Userinfo.valueOf(signUp.getUserinfo()))
