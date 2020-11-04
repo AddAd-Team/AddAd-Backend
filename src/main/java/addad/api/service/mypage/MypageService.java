70a18a5f46d7591817c1b04101b83e933f54f259
mspring03 <aespringaa@gmail.com>
2020-11-05 02:49:37 +0900

    [MODIFY] param

--- src/main/java/addad/api/service/mypage/MypageService.java
+++ src/main/java/addad/api/service/mypage/MypageService.java
@@ -1,6 +1,6 @@
 package addad.api.service.mypage;
 
 public interface MypageService {
-    void passwordAuth (String Email, String Password);
-    void passwordChange (String Email, String Password);
+    void passwordAuth (String Password);
+    void passwordChange (String Password);
 }
