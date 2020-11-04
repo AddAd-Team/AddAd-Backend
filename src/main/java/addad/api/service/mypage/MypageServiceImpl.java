e4a8c421ab3f7248ab455e4b579e32f32adc1b5e
mspring03 <aespringaa@gmail.com>
2020-11-05 02:49:49 +0900

    [MODIFY]

--- src/main/java/addad/api/service/mypage/MypageServiceImpl.java
+++ src/main/java/addad/api/service/mypage/MypageServiceImpl.java
@@ -1,28 +1,40 @@
 package addad.api.service.mypage;
 
+import addad.api.config.security.AuthDetails;
+import addad.api.config.security.AuthenticationFacade;
+import addad.api.config.security.JwtTokenProvider;
 import addad.api.domain.entities.User;
 import addad.api.domain.repository.UserRepository;
 import addad.api.exception.IncorrectPasswordException;
 import addad.api.exception.UserNotFoundException;
-import addad.api.utils.PasswordEncoder;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
 
+@Service
+@RequiredArgsConstructor
 public class MypageServiceImpl implements MypageService {
 
-    private UserRepository userRepository;
+    private final AuthenticationFacade authenticationFacade;
+    private final JwtTokenProvider jwtTokenProvider;
+    private final PasswordEncoder passwordEncoder;
+    private final UserRepository userRepository;
 
     @Override
-    public void passwordAuth (String Email, String Password) {
-        userRepository.findByEmail(Email)
-                .filter(data -> PasswordEncoder.checkPassword(data.getPassword(), Password))
+    public void passwordAuth (String Password) {
+        userRepository.findByEmail(authenticationFacade.getUserEmail())
+                .filter(data -> passwordEncoder.matches(Password, data.getPassword()))
                 .orElseThrow(IncorrectPasswordException::new);
     }
 
     @Override
-    public void passwordChange (String Email, String Password) {
-        User user = userRepository.findByEmail(Email)
+    public void passwordChange (String Password) {
+        User user = userRepository.findByEmail(authenticationFacade.getUserEmail())
                 .orElseThrow(UserNotFoundException::new);
-                
-        String password = PasswordEncoder.encode(Password);
+
+        String password = passwordEncoder.encode(Password);
         user.passwordChange(password);
+
+        userRepository.save(user);
     }
 }
