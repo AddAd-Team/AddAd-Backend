2f007ce8c672505673d19d09ed7c3fad04420ec7
mspring03 <aespringaa@gmail.com>
2020-11-04 22:07:07 +0900

    [MODIFY]

--- src/main/java/addad/api/service/post/PostService.java
+++ src/main/java/addad/api/service/post/PostService.java
@@ -1,9 +1,10 @@
 package addad.api.service.post;
 
 import addad.api.domain.payload.request.PostRequest;
+import org.springframework.web.multipart.MultipartFile;
 
 
 public interface PostService {
-    void write(PostRequest postRequest);
+    void write(PostRequest postRequest, MultipartFile file);
 
 }
