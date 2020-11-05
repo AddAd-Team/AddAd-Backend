3b054e31710d2e5ac1e2c6e86dcbd65027d63c28
mspring03 <aespringaa@gmail.com>
2020-11-05 17:03:33 +0900

    [ADD] modify profile

--- src/main/java/addad/api/controller/MypageController.java
+++ src/main/java/addad/api/controller/MypageController.java
@@ -1,18 +1,24 @@
 package addad.api.controller;
 
+import addad.api.domain.payload.request.ModifyProfile;
 import addad.api.domain.payload.response.ProfileResponse;
 import addad.api.service.mypage.MypageService;
+import addad.api.utils.S3Service;
 import lombok.RequiredArgsConstructor;
 import lombok.Value;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+import java.io.IOException;
 
 @RestController
 @RequestMapping("/api/mypage")
 @RequiredArgsConstructor
 public class MypageController {
     private final MypageService mypageService;
+    private final S3Service s3Service;
 
     @PostMapping(value = "/passwordAuth")
     public void passwordAuth(@RequestBody @Valid String password) {
@@ -27,5 +33,10 @@ public class MypageController {
     @GetMapping(value = "/profile")
     public ProfileResponse profile() {
         return mypageService.profile();
+    }
+
+    @PostMapping(value = "/modifyProfile")
+    public ProfileResponse modifyProfile(ModifyProfile modifyProfile) throws IOException {
+        return mypageService.ModifyProfile(modifyProfile);
     }
 }
