9e25f9ae617f5417531b6d75c82863b7f88799d9
mspring03 <aespringaa@gmail.com>
2020-11-06 11:56:33 +0900

    [MODIFY]

--- src/main/java/addad/api/exception/PostNotFoundException.java
+++ src/main/java/addad/api/exception/PostNotFoundException.java
@@ -1,11 +1,12 @@
 package addad.api.exception;
 
+import addad.api.error.Exception.BusinessException;
+import addad.api.error.Exception.ErrorCode;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(HttpStatus.NOT_FOUND)
-public class PostNotFoundException extends RuntimeException {
+public class PostNotFoundException extends BusinessException {
     public PostNotFoundException(){
-        super("Post Not Found");
+        super(ErrorCode.POST_NOT_FOUND);
     }
 }
