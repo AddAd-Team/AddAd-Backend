942cd2850dc4d00bd959edf0d1fe7a4ec29bbfdc
mspring03 <aespringaa@gmail.com>
2020-11-06 11:56:40 +0900

    [MODIFY]

--- src/main/java/addad/api/exception/UserNotFoundException.java
+++ src/main/java/addad/api/exception/UserNotFoundException.java
@@ -5,7 +5,6 @@ import addad.api.error.Exception.ErrorCode;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(HttpStatus.NOT_FOUND)
 public class UserNotFoundException extends BusinessException {
     public UserNotFoundException() {
         super(ErrorCode.USER_NOT_LEADER);
