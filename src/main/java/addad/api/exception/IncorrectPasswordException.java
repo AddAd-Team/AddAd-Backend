a2610ec74ca9dd8b76e2f24623575d44938408ea
mspring03 <aespringaa@gmail.com>
2020-11-06 11:56:26 +0900

    [MODIFY]

--- src/main/java/addad/api/exception/IncorrectPasswordException.java
+++ src/main/java/addad/api/exception/IncorrectPasswordException.java
@@ -6,7 +6,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import addad.api.error.Exception.ErrorCode;
 
-@ResponseStatus(HttpStatus.BAD_REQUEST)
 public class IncorrectPasswordException extends BusinessException {
 
     public IncorrectPasswordException() {
