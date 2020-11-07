66fe09dc546041094a091303f459b1496d8fc2d5
mspring03 <aespringaa@gmail.com>
2020-11-07 17:30:34 +0900

    [MODIFY]

--- src/main/java/addad/api/domain/repository/LikeRepository.java
+++ src/main/java/addad/api/domain/repository/LikeRepository.java
@@ -1,7 +1,8 @@
 package addad.api.domain.repository;
 
+import addad.api.domain.entities.Likes;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface LikeRepository extends JpaRepository<Like, Integer> {
-    Like findByPostIdAndUserId(Integer likeId, Integer userId);
+public interface LikeRepository extends JpaRepository<Likes, Integer> {
+    Likes findByPostIdAndUserId(Integer likeId, Integer userId);
 }
