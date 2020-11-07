97fe413b3e64a5952d177a403b742c8e7ef19f7a
mspring03 <aespringaa@gmail.com>
2020-11-07 17:01:42 +0900

    [MODIFY]

--- src/main/java/addad/api/domain/repository/PostRepository.java
+++ src/main/java/addad/api/domain/repository/PostRepository.java
@@ -1,11 +1,8 @@
 package addad.api.domain.repository;
 
-import addad.api.domain.entities.Post;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.Optional;
 
 
 public interface PostRepository extends JpaRepository<Post, Integer> {
