45fe0189b9eb4b429b867a47d2ec45660d5e8398
mspring03 <aespringaa@gmail.com>
2020-11-07 21:22:28 +0900

    [MODIFY] import Change route

--- src/main/java/addad/api/domain/repository/LikeRepository.java
+++ src/main/java/addad/api/domain/repository/LikeRepository.java
@@ -3,6 +3,6 @@ package addad.api.domain.repository;
 import addad.api.domain.entities.Likes;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface LikeRepository extends JpaRepository<Likes, Integer> {
-    Likes findByPostIdAndUserId(Integer likeId, Integer userId);
+public interface LikeRepository extends JpaRepository<Likes, Long> {
+//    Likes findByPostIdAndUserId(Integer likeId, Integer userId);
 }
