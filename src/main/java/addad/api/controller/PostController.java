e00ca7df06b0401f839cab4b991b55ab0e3d11a0
mspring03 <aespringaa@gmail.com>
2020-11-07 17:01:31 +0900

    [MODIFY]

--- src/main/java/addad/api/controller/PostController.java
+++ src/main/java/addad/api/controller/PostController.java
@@ -1,22 +1,15 @@
 package addad.api.controller;
 
-import addad.api.domain.entities.Post;
 import addad.api.domain.payload.request.PostRequest;
 import addad.api.domain.payload.response.FeedResponse;
-import addad.api.domain.payload.response.PostResponse;
-import addad.api.domain.repository.PostRepository;
 import addad.api.service.post.PostService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
 
-import javax.validation.Valid;
 import java.util.List;
 
 @RestController
