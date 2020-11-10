e2aba40ca12c13661a8002fffe82f33aac8055df
mspring03 <mspringmspring@naver.com>
2020-11-10 23:00:18 +0900

    [ADD] findOneUserById

--- src/main/java/addad/api/service/search/SearchService.java
+++ src/main/java/addad/api/service/search/SearchService.java
@@ -3,13 +3,16 @@ package addad.api.service.search;
 import addad.api.domain.entities.User;
 import addad.api.domain.payload.request.SearchName;
 import addad.api.domain.payload.request.SearchTag;
+import addad.api.domain.payload.response.ProfileResponse;
+import addad.api.domain.payload.response.SearchResponse;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 
 public interface SearchService {
-    List<User> creatorSearchBasic(Pageable pageable);
-    List<User> creatorSearchByName(Pageable pageable, SearchName searchName);
-    List<User> creatorSearchByTag(Pageable pageable, SearchTag searchTag);
+    List<SearchResponse> creatorSearchBasic(Pageable pageable);
+    List<SearchResponse> creatorSearchByName(Pageable pageable, SearchName searchName);
+    List<SearchResponse> creatorSearchByTag(Pageable pageable, SearchTag searchTag);
+    ProfileResponse findOneUserById(Long Id);
 }
