090f8176ef4df1853397a72e5244125bde998e14
Kiwon <parkkiwon0424@gmail.com>
2020-11-11 15:45:15 +0900

    [MODIFY]

--- src/main/java/addad/api/controller/PostController.java
+++ src/main/java/addad/api/controller/PostController.java
@@ -1,12 +1,12 @@
 package addad.api.controller;
 
 import addad.api.domain.payload.request.PostRequest;
+import addad.api.domain.payload.response.DetailFeedResponse;
 import addad.api.domain.payload.response.FeedResponse;
 import addad.api.service.post.PostService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
-import org.springframework.http.HttpStatus;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
@@ -20,14 +20,18 @@ public class PostController {
 
     private final PostService postService;
 
-    @ResponseStatus(HttpStatus.OK)
     @PostMapping(value = "/write")
     public void write(PostRequest postRequest) {
         postService.write(postRequest);
     }
 
     @GetMapping("/feed")
-    public List<FeedResponse> getFeed(@PageableDefault(sort = {"createdAt"}, size = 3) Pageable pageable) {
+    public List<FeedResponse> feed(@PageableDefault(sort = {"createdAt"}, size = 10) Pageable pageable) {
         return postService.getFeed(pageable);
+    }
+
+    @GetMapping("feed/{id}")
+    public DetailFeedResponse detailFeed(@PathVariable Long id){
+        return postService.getDetailFeed(id);
     }
 }
