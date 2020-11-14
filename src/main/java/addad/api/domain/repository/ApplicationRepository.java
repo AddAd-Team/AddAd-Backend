c13e6c22d63da015605a1194e3dbe806c9bb8dcb
mspring03 <mspringmspring@naver.com>
2020-11-15 03:29:27 +0900

    [MODIFY]

--- src/main/java/addad/api/domain/repository/ApplicationRepository.java
+++ src/main/java/addad/api/domain/repository/ApplicationRepository.java
@@ -5,8 +5,9 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface ApplicationRepository extends JpaRepository<Application, Long> {
     List<Application> findAllByUser_id(Long user_id);
-    Application findByUser_idAndAndPost_id(Long user_id, Long post_id);
+    Optional<Application> findByUser_idAndAndPost_id(Long user_id, Long post_id);
 }
