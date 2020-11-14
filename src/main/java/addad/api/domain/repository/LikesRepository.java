3acec2766e5e76a8295e266cb3104dec3e844a5d
mspring03 <mspringmspring@naver.com>
2020-11-15 03:29:33 +0900

    [MODIFY]

--- src/main/java/addad/api/domain/repository/LikesRepository.java
+++ src/main/java/addad/api/domain/repository/LikesRepository.java
@@ -3,6 +3,8 @@ package addad.api.domain.repository;
 import addad.api.domain.entities.Likes;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface LikesRepository extends JpaRepository<Likes, Long> {
-    Likes findByUser_idAndAndPost_id(Long user_id, Long post_id);
+    Optional<Likes> findByUser_idAndAndPost_id(Long user_id, Long post_id);
 }
