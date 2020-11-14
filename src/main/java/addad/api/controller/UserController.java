d93d2df1bb612ce5b5d4f62575f2df0c119d8ebe
mspring03 <mspringmspring@naver.com>
2020-11-15 02:13:29 +0900

    [MODIFY]

--- src/main/java/addad/api/controller/UserController.java
+++ src/main/java/addad/api/controller/UserController.java
@@ -23,17 +23,17 @@ public class UserController {
     private final EmailService emailService;
     private final S3Service s3Service;
 
-    @PostMapping(value = "/emailSender")
+    @PostMapping("/emailSender")
     public void emailSender(@RequestBody() @NotNull @Email userEmail email) {
         emailService.sendEmail(email.getEmail());
     }
 
-    @PutMapping(value = "/emailAuth")
+    @PutMapping("/emailAuth")
     public void verifyEmail(@RequestBody @Valid VerifyCodeRequest verifyCodeRequest) {
         emailService.verifyEmail(verifyCodeRequest);
     }
 
-    @PostMapping(value = "/signup")
+    @PostMapping("/signup")
     public void signUp(@RequestBody @Valid SignUp signUp) {
         userService.signUp(signUp);
     }
