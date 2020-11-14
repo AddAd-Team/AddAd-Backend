2b9dcafb692f6b59592269cb2cae9ec3e4ffffba
mspring03 <mspringmspring@naver.com>
2020-11-15 02:14:28 +0900

    [MODIFY] likes table

--- src/main/java/addad/api/domain/entities/Likes.java
+++ src/main/java/addad/api/domain/entities/Likes.java
@@ -1,16 +1,12 @@
 package addad.api.domain.entities;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import lombok.ToString;
+import lombok.*;
 
 import javax.persistence.*;
 
 @Getter
 @Setter
-@ToString(exclude = "post")
 @Entity
 @NoArgsConstructor
 public class Likes {
@@ -22,8 +18,22 @@ public class Likes {
     @Column()
     private Long post_id;
 
+    @Column()
+    private Long user_id;
+
+    @ManyToOne
+    @JoinColumn(name="user_id", insertable = false, updatable = false)
+    @JsonIgnore
+    private User user;
+
     @ManyToOne
     @JoinColumn(name="post_id", insertable = false, updatable = false)
     @JsonIgnore
     private Post post;
+
+    @Builder
+    Likes(Long user_id, Long post_id) {
+        this.user_id = user_id;
+        this.post_id = post_id;
+    }
 }
