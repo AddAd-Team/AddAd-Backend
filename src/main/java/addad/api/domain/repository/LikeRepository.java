1a74d3cf1782e1813f424cbab8729ea04cddf008
mspring03 <mspringmspring@naver.com>
2020-11-14 15:55:17 +0900

    [MODIFY]

--- src/main/java/addad/api/domain/repository/LikeRepository.java
+++ src/main/java/addad/api/domain/repository/LikeRepository.java
@@ -1,8 +1,8 @@
-package addad.api.domain.repository;
-
-import addad.api.domain.entities.Likes;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface LikeRepository extends JpaRepository<Likes, Long> {
-//    Likes findByPostIdAndUserId(Integer likeId, Integer userId);
-}
+//package addad.api.domain.repository;
+//
+//import addad.api.domain.entities.Likes;
+//import org.springframework.data.jpa.repository.JpaRepository;
+//
+//public interface LikeRepository extends JpaRepository<Likes, Long> {
+////    Likes findByPostIdAndUserId(Integer likeId, Integer userId);
+//}
