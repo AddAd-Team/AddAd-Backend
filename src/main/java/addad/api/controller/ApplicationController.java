8765b1e478ec1f00e206136f60eb8522695b6c7e
mspring03 <mspringmspring@naver.com>
2020-11-16 10:20:18 +0900

    [ADD] application, apply

--- src/main/java/addad/api/controller/ApplicationController.java
+++ src/main/java/addad/api/controller/ApplicationController.java
@@ -1,16 +1,12 @@
 package addad.api.controller;
 
-import addad.api.domain.payload.request.SignIn;
 import addad.api.domain.payload.response.ApplicationResponse;
-import addad.api.domain.payload.response.SearchResponse;
-import addad.api.domain.payload.response.TokenResponse;
 import addad.api.service.application.ApplicationService;
-import addad.api.service.auth.AuthService;
+import addad.api.service.post.PostService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
-import javax.validation.constraints.NotNull;
+import javax.validation.Valid;
 import java.util.List;
 
 @RestController
@@ -19,8 +15,13 @@ import java.util.List;
 public class ApplicationController {
     private final ApplicationService applicationService;
 
-    @GetMapping("basic")
-    public List<ApplicationResponse> applicationApply() {
-        return applicationService.application();
+    @GetMapping("basic/{postId}")
+    public List<ApplicationResponse> applicationApply(@PathVariable Long postId) {
+        return applicationService.application(postId);
+    }
+
+    @PostMapping("/apply")
+    public void apply(@PathVariable Long postId) {
+        applicationService.apply(postId);
     }
 }
