97342cf0c08b28c61a3f19f23d7dcc221346496d
mspring03 <mspringmspring@naver.com>
2020-11-17 15:39:00 +0900

    [MODIFY]

--- src/main/java/addad/api/service/application/ApplicationServiceImpl.java
+++ src/main/java/addad/api/service/application/ApplicationServiceImpl.java
@@ -1,6 +1,7 @@
 package addad.api.service.application;
 
 import addad.api.domain.entities.Application;
+import addad.api.domain.entities.Likes;
 import addad.api.domain.entities.User;
 import addad.api.domain.entities.enums.Userinfo;
 import addad.api.domain.payload.response.ApplicationResponse;
@@ -16,6 +17,7 @@ import addad.api.config.security.AuthenticationFacade;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -27,17 +29,15 @@ public class ApplicationServiceImpl implements ApplicationService{
     private final DefaultImg defaultImg;
 
     @Override
-    public List<ApplicationResponse> application() {
-        User user = userRepository.findByEmail(authenticationFacade.getUserEmail())
-                .orElseThrow(UserNotFoundException::new);
+    public List<ApplicationResponse> application(Long post_id) {
 
-        List<Application> applications = applicationRepository.findAllByUser_id(user.getId());
+        List<Application> applications = applicationRepository.findAllByPost_id(post_id);
 
         List<ApplicationResponse> userList = new ArrayList<>();
         for (Application application : applications) {
             userList.add(
                     ApplicationResponse.builder()
-                            .id(application.getId())
+                            .user_id(application.getUser_id())
                             .name(application.getUser().getName())
                             .profileImg(defaultImg.basic(application.getUser().getProfileImg()))
                             .build()
@@ -45,5 +45,32 @@ public class ApplicationServiceImpl implements ApplicationService{
         }
 
         return userList;
+    }
+
+    @Override
+    public void apply(Long Id) {
+        User user = userRepository.findByEmail(authenticationFacade.getUserEmail())
+                .orElseThrow(UserNotFoundException::new);
+
+        Optional<Application> application = applicationRepository.findByUser_idAndAndPost_id(user.getId(), Id);
+
+        if(!application.isPresent()) {
+            applicationRepository.save(
+                    Application.builder()
+                            .user_id(user.getId())
+                            .post_id(Id)
+                            .build()
+            );
+        }
+    }
+
+    @Override
+    public void applicationDelete(Long postId){
+        User user = userRepository.findByEmail(authenticationFacade.getUserEmail())
+                .orElseThrow(UserNotFoundException::new);
+
+        Application application = applicationRepository.findByUser_idAndPost_id(user.getId(), postId);
+
+        applicationRepository.delete(application);
     }
 }
