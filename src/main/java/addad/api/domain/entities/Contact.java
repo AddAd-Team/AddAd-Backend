6291691da6ff40cd4fa450539433ebc502484c4f
mspring03 <mspringmspring@naver.com>
2020-11-17 23:05:33 +0900

    [CREATE] contact table

---
+++ src/main/java/addad/api/domain/entities/Contact.java
@@ -0,0 +1,51 @@
+package addad.api.domain.entities;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@Entity
+@NoArgsConstructor
+public class Contact {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column()
+    private Long post_id;
+
+    @Column()
+    private Long creator_id;
+
+    @Column()
+    private Long advertiser_id;
+
+    @ManyToOne
+    @JoinColumn(name="creator_id", insertable = false, updatable = false)
+    @JsonIgnore
+    private User creator;
+
+    @ManyToOne
+    @JoinColumn(name="advertiser_id", insertable = false, updatable = false)
+    @JsonIgnore
+    private User advertiser;
+
+    @ManyToOne
+    @JoinColumn(name="post_id", insertable = false, updatable = false)
+    @JsonIgnore
+    private Post post;
+
+    @Builder
+    Contact(Long creator_id, Long advertiser_id, Long post_id) {
+        this.creator_id = creator_id;
+        this.advertiser_id = advertiser_id;
+        this.post_id = post_id;
+    }
+}
