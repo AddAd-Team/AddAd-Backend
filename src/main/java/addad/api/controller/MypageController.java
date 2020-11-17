c4877008d2b56e6f01c5f31276b2c214c441b02a
mspring03 <mspringmspring@naver.com>
2020-11-17 23:07:14 +0900

    [MODIFY]

--- src/main/java/addad/api/controller/MypageController.java
+++ src/main/java/addad/api/controller/MypageController.java
@@ -1,18 +1,13 @@
 package addad.api.controller;
 
-import addad.api.domain.payload.request.ModifyPost;
 import addad.api.domain.payload.request.ModifyProfile;
-import addad.api.domain.payload.response.ADListResponse;
 import addad.api.domain.payload.response.ProfileResponse;
 import addad.api.service.mypage.MypageService;
 import addad.api.utils.S3Service;
 import lombok.RequiredArgsConstructor;
-import lombok.Value;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
 
 import javax.validation.Valid;
-import javax.validation.constraints.NotNull;
 import java.io.IOException;
 import java.util.List;
 
