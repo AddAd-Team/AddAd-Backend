5cc350984bf4809b6d899173271b7c100642ed69
mspring03 <mspringmspring@naver.com>
2020-11-17 23:06:15 +0900

    [ADD] findAllbyUserId query

--- src/main/java/addad/api/domain/repository/LikesRepository.java
+++ src/main/java/addad/api/domain/repository/LikesRepository.java
@@ -3,9 +3,11 @@ package addad.api.domain.repository;
 import addad.api.domain.entities.Likes;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface LikesRepository extends JpaRepository<Likes, Long> {
     Optional<Likes> findByUser_idAndAndPost_id(Long user_id, Long post_id);
     Likes findByUser_idAndPost_id(Long user_id, Long post_id);
+    List<Likes> findAllByUser_id(Long user_id);
 }
