2bc151cb62f92c0657ea4f35b32682d5d998d6fa
mspring03 <mspringmspring@naver.com>
2020-11-17 23:07:27 +0900

    [MODIFY]

--- src/main/java/addad/api/service/mypage/MypageService.java
+++ src/main/java/addad/api/service/mypage/MypageService.java
@@ -2,14 +2,14 @@ package addad.api.service.mypage;
 
 import addad.api.domain.payload.request.ModifyProfile;
 import addad.api.domain.payload.response.ProfileResponse;
-import org.springframework.web.multipart.MultipartFile;
-import software.amazon.ion.IonException;
 
 import java.io.IOException;
+import java.util.List;
 
 public interface MypageService {
     void passwordAuth (String Password);
     void passwordChange (String Password);
     ProfileResponse profile ();
     ProfileResponse ModifyProfile(ModifyProfile modifyProfile) throws IOException;
+    List<ADListResponse> likeAd();
 }
