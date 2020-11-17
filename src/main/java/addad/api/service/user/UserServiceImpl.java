4c5e2c48d22f6a9742bc1d318f2e62564252dce3
mspring03 <mspringmspring@naver.com>
2020-11-17 15:39:59 +0900

    [ADD] test

--- src/main/java/addad/api/service/user/UserServiceImpl.java
+++ src/main/java/addad/api/service/user/UserServiceImpl.java
@@ -8,6 +8,14 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import javax.persistence.criteria.CriteriaBuilder;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.TemporalAccessor;
+import java.util.Date;
+
 
 @Service
 @RequiredArgsConstructor
@@ -27,5 +35,23 @@ public class UserServiceImpl implements UserService {
                         .profileImg("")
                         .build()
         );
+    }
+
+    @Override
+    public void test() {
+        String strStartDate = "20210104";
+        String strEndDate = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyyMMdd"));
+        String strFormat = "yyyyMMdd";
+
+        SimpleDateFormat sdf = new SimpleDateFormat(strFormat);
+        try{
+            Date startDate = sdf.parse(strStartDate);
+            Date endDate = sdf.parse(strEndDate);
+
+            long diffDay = (startDate.getTime() - endDate.getTime()) / (24*60*60*1000);
+            System.out.println(diffDay+"일");
+        }catch(ParseException e){
+            e.printStackTrace();
+        }
     }
 }
