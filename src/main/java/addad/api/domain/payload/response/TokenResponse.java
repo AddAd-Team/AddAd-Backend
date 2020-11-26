3b5dc9b5befd967786886f37c3c57c38c43e5e0d
mspring03 <mspringmspring@naver.com>
2020-11-26 09:03:47 +0900

    [ADD] Return userinfo at login

--- src/main/java/addad/api/domain/payload/response/TokenResponse.java
+++ src/main/java/addad/api/domain/payload/response/TokenResponse.java
@@ -1,9 +1,12 @@
 package addad.api.domain.payload.response;
 
+import addad.api.domain.entities.enums.Userinfo;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.Setter;
 
 @Getter
+@Setter
 @Builder
 public class TokenResponse {
 
@@ -12,5 +15,7 @@ public class TokenResponse {
     private String refreshToken;
 
     private String tokenType;
+
+    private Userinfo userinfo;
 }
 
