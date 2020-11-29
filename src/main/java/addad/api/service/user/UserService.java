14b89dbbaaac3c256f018f2bf2a03ca632f9f483
mspring03 <mspringmspring@naver.com>
2020-11-29 23:01:11 +0900

    [MODIFY] </user/UserService.java>

--- src/main/java/addad/api/service/user/UserService.java
+++ src/main/java/addad/api/service/user/UserService.java
@@ -2,8 +2,10 @@ package addad.api.service.user;
 
 import addad.api.domain.payload.request.SignUp;
 
+import java.io.IOException;
+
 public interface UserService {
 
     void signUp(SignUp signUp);
-    void test();
+    void test() throws IOException;
 }
