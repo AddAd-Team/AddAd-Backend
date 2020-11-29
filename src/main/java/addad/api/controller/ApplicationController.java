5aa6560c5415eb5de678bf4b3a63f08025d2508d
mspring03 <mspringmspring@naver.com>
2020-11-29 22:48:29 +0900

    [ADD] </controller/ApplicationController.java> application allow api

--- src/main/java/addad/api/controller/ApplicationController.java
+++ src/main/java/addad/api/controller/ApplicationController.java
@@ -1,10 +1,13 @@
 package addad.api.controller;
 
+import addad.api.domain.payload.request.ApplicationAllow;
 import addad.api.domain.payload.response.ApplicationResponse;
 import addad.api.service.application.ApplicationService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
+import java.io.IOException;
 import java.util.List;
 
 @RestController
@@ -21,6 +24,11 @@ public class ApplicationController {
     @PostMapping("/apply/{postId}")
     public void apply(@PathVariable Long postId) {
         applicationService.apply(postId);
+    }
+
+    @PostMapping("/allow")
+    public void allow(@RequestBody @Valid ApplicationAllow applicationAllow) throws IOException {
+        applicationService.applicationAllow(applicationAllow.getUserList(), applicationAllow.getPostId());
     }
 
     @DeleteMapping("/apply/{postId}")
