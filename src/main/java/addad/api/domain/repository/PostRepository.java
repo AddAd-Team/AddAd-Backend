7296f39f9a6fcc30e79631392300b0a629a49b54
mspring03 <mspringmspring@naver.com>
2020-11-30 16:43:46 +0900

    [MODIFY] </repository/PostRepository.java>

--- src/main/java/addad/api/domain/repository/PostRepository.java
+++ src/main/java/addad/api/domain/repository/PostRepository.java
@@ -1,6 +1,7 @@
 package addad.api.domain.repository;
 
 import addad.api.domain.entities.Post;
+import addad.api.domain.entities.enums.PostStatus;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -10,7 +11,7 @@ import java.util.List;
 import java.util.Optional;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
-    Page<Post> findAllBy(Pageable pageable);
+    Page<Post> findAllByPostStatus(Pageable pageable, PostStatus postStatus);
     Optional<Post> findById(Long id);
-    List<Post> findByUserId(Long userId);
+    List<Post> findByUserIdAndPostStatusOrPostStatus(Long userId, PostStatus firstStatus, PostStatus secondStatus);
 }
