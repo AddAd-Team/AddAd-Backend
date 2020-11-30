08d06d42065bc7e7d1bd04bc5786633be212ad24
mspring03 <mspringmspring@naver.com>
2020-11-30 23:22:52 +0900

    [CREATE] </repository/NotificationRepository.java>

---
+++ src/main/java/addad/api/domain/repository/NotificationRepository.java
@@ -0,0 +1,10 @@
+package addad.api.domain.repository;
+
+import addad.api.domain.entities.Notification;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface NotificationRepository extends JpaRepository<Notification, Long> {
+    List<Notification> findAllByUserId(Long userId);
+}
