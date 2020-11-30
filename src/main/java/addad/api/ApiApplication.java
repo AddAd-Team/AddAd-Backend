eaeb4fe9c98f4bdb2f89077562e87edabe08f875
mspring03 <mspringmspring@naver.com>
2020-11-30 16:35:42 +0900

    [ADD] </api/Application.java> EnableAsync annotation

--- src/main/java/addad/api/ApiApplication.java
+++ src/main/java/addad/api/ApiApplication.java
@@ -3,8 +3,10 @@ package addad.api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @EnableCaching
+@EnableAsync
 @SpringBootApplication
 public class ApiApplication {
 
