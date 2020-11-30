7edc47309530507aee9c8330a70698ae1456137e
mspring03 <mspringmspring@naver.com>
2020-11-30 23:20:51 +0900

    [CREATE] </entities/Notification.java> notification entity

---
+++ src/main/java/addad/api/domain/entities/Notification.java
@@ -0,0 +1,35 @@
+package addad.api.domain.entities;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Setter
+@Getter
+@Entity
+@NoArgsConstructor
+public class Notification {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "user_id")
+    private Long userId;
+
+    @Column
+    private String profileImg;
+
+    @Column
+    private String description;
+
+    @Builder
+    public Notification(Long userId, String profileImg, String description) {
+        this.userId = userId;
+        this.profileImg = profileImg;
+        this.description = description;
+    }
+}
